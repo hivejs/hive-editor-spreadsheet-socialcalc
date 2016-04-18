65ad02866df62436e22fc1eb5b267667d7591ce0
Marcel Klehr <mklehr@gmx.net>
2016-04-18 21:20:51 +0200

    Fix editor height, for now

--- client.js
+++ client.js
@@ -60,7 +60,7 @@ function setup(plugin, imports, register) {
     function resizeEditor() {
       el.firstChild.style['display'] = 'none'
       var constraints = el.getBoundingClientRect()
-      socialCalcControl.editor.ResizeTableEditor(constraints.width, constraints.height)
+      socialCalcControl.editor.ResizeTableEditor(constraints.width, constraints.height-150)
       el.firstChild.style['display'] = 'block'
     }
 
